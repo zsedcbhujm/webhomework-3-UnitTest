63412aaefb3e1cd32fa7cced3092a040869a330e
gxy <xy_guo@foxmail.com>
2020-06-23 23:45:14 +0800

    init program with readme

---
+++ TexasPoker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TexasPoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
